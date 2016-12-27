fecca38614f70184ebe9adec29c10c99197f8f58
Jong-hyouk Yun <jong-hyouk.yun@zalando.de>
2016-12-27 15:13:16 +0100

    Docstrings for bindings.

--- thompson/bindings.py
+++ thompson/bindings.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8; -*-
+"""Bindings.
+
+Used to hold variables in expression-language. Also takes care of
+binding-inheritances.
+"""
 
 
 class Binding(object):
+    """Binding class.
+
+    Each stackframe has own Binding, and Binding can has a parent.
+    """
     def __init__(self, parent=None):
+        """If `parent` parameter is absent, creates a root-binding."""
         self.parent = parent
         self.b = {}
 
     def contains(self, k):
+        """Returns true/false, looking for `k` in whole inheritance-tree."""
         haz = self.contains_no_inherits(k)
         if haz:
             return haz
@@ -17,9 +28,17 @@ class Binding(object):
             return haz
 
     def contains_no_inherits(self, k):
+        """Returns true/false, but looking in this Binding, not on its
+        parent."""
         return k in self.b
 
     def get(self, k):
+        """Returns a value stored with `k` as a key,
+        Looking on whole inheritance-tree.
+
+        If its binding-tree doesn't have any matching key, it raises a
+        `NameError` on that key.
+        """
         if self.contains(k):
             cur = self
             while True:
@@ -33,26 +52,34 @@ class Binding(object):
             raise NameError(k)
 
     def get_no_inherits(self, k):
+        """Look for `k` in this binding, without inheritance.
+
+        No key is associated, raises `NameError` on `k`."""
         if k in self.b:
             return self.b[k]
         else:
             raise NameError(k)
 
     def set(self, k, v):
+        """Simply sets a keyed value in this binding."""
         self.b[k] = v
 
     def set_uplevel(self, k, v):
+        """Sets association in parent binding, if there's no parent, raises a
+        `ValueError`.
+        """
         if self.parent is None:
             raise ValueError("there is no parent for this binding!")
         else:
             self.parent.set(k, v)
 
     def set_global(self, k, v):
+        """Sets association in root-binding. If there's no parent, then this
+        is a root-binding."""
         cur = self
         while True:
             if cur.parent is None:
                 break
             else:
                 cur = cur.parent
-        #
         cur.set(k, v)
