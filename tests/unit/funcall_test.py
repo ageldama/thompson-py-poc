3b7fcd20d57ae7a80233f4cb326aa8f7a6fa7db0
Jong-hyouk Yun <jong-hyouk.yun@zalando.de>
2016-12-29 19:08:06 +0100

    Fix the funcall+compose bug.

--- tests/unit/funcall_test.py
+++ tests/unit/funcall_test.py
@@ -5,9 +5,8 @@ from thompson.literals import StringVal, NumberVal
 from thompson.literals import FunctionParamVal, FunctionVal
 from thompson.builtin_operators import Assign, AssignGlobal, AssignUpvar
 from thompson.builtin_operators import Funcall
-from thompson.builtin_operators import ArithPlus, ArithMult, BindingRef
+from thompson.builtin_operators import ArithPlus, BindingRef
 from thompson.evaluators import evaluate
-import pytest
 
 
 def test_simple_adder():
@@ -113,50 +112,3 @@ def test_incx():
     #
     result = evaluate(c, Funcall(BindingRef('inc42'), [N(1)]))
     assert result == N(43)
-
-
-def define_compose1(c):
-    """compose two 1-arity functions into a function."""
-    params_expr = [FunctionParamVal('g'), FunctionParamVal('f')]
-    body = FunctionVal([FunctionParamVal('x')],
-                       Funcall(BindingRef('g'),
-                               [Funcall(BindingRef('f'), [BindingRef('x')])]))
-    return evaluate(c, Assign('compose1', FunctionVal(params_expr, body)))
-
-
-def define_inc1(c):
-    N = NumberVal
-    params = [FunctionParamVal('x')]
-    body = ArithPlus(N(1), BindingRef('x'))
-    return evaluate(c, Assign('inc1', FunctionVal(params, body)))
-
-
-def define_square(c):
-    params = [FunctionParamVal('x')]
-    body = ArithMult(BindingRef('x'), BindingRef('x'))
-    return evaluate(c, Assign('square', FunctionVal(params, body)))
-
-
-@pytest.mark.skip
-def test_func_composition():
-    """Test on function-value as return-value and parameter to another
-    function."""
-    N = NumberVal
-    b_root = Binding()
-    c_root = Context(b_root)
-    # funcs.
-    define_compose1(c_root)
-    define_inc1(c_root)
-    assert evaluate(c_root, Funcall(BindingRef('inc1'), [N(1)])) == N(2)
-    define_square(c_root)
-    assert evaluate(c_root, Funcall(BindingRef('square'), [N(4)])) == N(16)
-    # compose - square(inc1(x))
-    evaluate(c_root, Assign('my_fun',
-                            Funcall(BindingRef('compose1'),
-                                    [BindingRef('square'),
-                                     BindingRef('inc1')])))
-    evaluate(c_root, BindingRef('my_fun'))
-    # test it.
-    # TODO: FIXME:
-    result = evaluate(c_root, Funcall(BindingRef('my_fun'), [N(2)]))
-    assert result == N(9)
