3b19ae3f0a9b5b0b3ab697cc51e81daa6720a080
Jong-hyouk Yun <jong-hyouk.yun@zalando.de>
2016-12-29 15:21:42 +0100

    Tests on `funcall`.

--- tests/unit/funcall_test.py
+++ tests/unit/funcall_test.py
@@ -5,11 +5,13 @@ from thompson.literals import StringVal, NumberVal
 from thompson.literals import FunctionParamVal, FunctionVal
 from thompson.builtin_operators import Assign, AssignGlobal, AssignUpvar
 from thompson.builtin_operators import Funcall
-from thompson.builtin_operators import ArithPlus, BindingRef
+from thompson.builtin_operators import ArithPlus, ArithMult, BindingRef
 from thompson.evaluators import evaluate
+import pytest
 
 
 def test_simple_adder():
+    """A function can take parameters and evaluates to a value."""
     S, N = StringVal, NumberVal
     b = Binding()
     b.set('x', 42)
@@ -27,6 +29,8 @@ def test_simple_adder():
 
 
 def test_closure1_inc():
+    """A function has a closure, a binding.
+    And can access to its variables."""
     N = NumberVal
     b_root = Binding()
     # set inc = fun {|x| ... }
@@ -50,6 +54,7 @@ def test_closure1_inc():
 
 
 def test_closure2_counter():
+    """A function can (re)assign variables in a closure with `AssignUpvar`."""
     N = NumberVal
     b_root = Binding()
     # set counter = fun {|x| ... }
@@ -78,6 +83,7 @@ def test_closure2_counter():
 
 
 def test_make_adder():
+    """A function can be referenced as a variable."""
     S, N = StringVal, NumberVal
     b = Binding()
     c = Context(b)
@@ -90,5 +96,67 @@ def test_make_adder():
     assert result == N(7)
 
 
+def define_make_incx(c):
+    params_expr = [FunctionParamVal('x')]
+    body = FunctionVal([FunctionParamVal('n')],
+                       ArithPlus(BindingRef('x'), BindingRef('n')))
+    return evaluate(c, Assign('make_incx', FunctionVal(params_expr, body)))
+
+
+def test_incx():
+    N = NumberVal
+    b = Binding()
+    c = Context(b)
+    #
+    define_make_incx(c)
+    evaluate(c, Assign('inc42', Funcall(BindingRef('make_incx'), [N(42)])))
+    #
+    result = evaluate(c, Funcall(BindingRef('inc42'), [N(1)]))
+    assert result == N(43)
+
+
+def define_compose1(c):
+    """compose two 1-arity functions into a function."""
+    params_expr = [FunctionParamVal('g'), FunctionParamVal('f')]
+    body = FunctionVal([FunctionParamVal('x')],
+                       Funcall(BindingRef('g'),
+                               [Funcall(BindingRef('f'), [BindingRef('x')])]))
+    return evaluate(c, Assign('compose1', FunctionVal(params_expr, body)))
+
+
+def define_inc1(c):
+    N = NumberVal
+    params = [FunctionParamVal('x')]
+    body = ArithPlus(N(1), BindingRef('x'))
+    return evaluate(c, Assign('inc1', FunctionVal(params, body)))
+
+
+def define_square(c):
+    params = [FunctionParamVal('x')]
+    body = ArithMult(BindingRef('x'), BindingRef('x'))
+    return evaluate(c, Assign('square', FunctionVal(params, body)))
+
+
+@pytest.mark.skip
 def test_func_composition():
-    pass
+    """Test on function-value as return-value and parameter to another
+    function."""
+    N = NumberVal
+    b_root = Binding()
+    c_root = Context(b_root)
+    # funcs.
+    define_compose1(c_root)
+    define_inc1(c_root)
+    assert evaluate(c_root, Funcall(BindingRef('inc1'), [N(1)])) == N(2)
+    define_square(c_root)
+    assert evaluate(c_root, Funcall(BindingRef('square'), [N(4)])) == N(16)
+    # compose - square(inc1(x))
+    evaluate(c_root, Assign('my_fun',
+                            Funcall(BindingRef('compose1'),
+                                    [BindingRef('square'),
+                                     BindingRef('inc1')])))
+    evaluate(c_root, BindingRef('my_fun'))
+    # test it.
+    # TODO: FIXME:
+    result = evaluate(c_root, Funcall(BindingRef('my_fun'), [N(2)]))
+    assert result == N(9)
