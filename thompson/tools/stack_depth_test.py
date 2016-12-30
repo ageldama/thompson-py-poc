70ef332d291f676de38b6d06a4c367eef0bae7d2
Jong-hyouk Yun <jong-hyouk.yun@zalando.de>
2016-12-30 14:06:26 +0100

    Move `stack_depth_test` as a standalone script.

--- thompson/tools/stack_depth_test.py
+++ thompson/tools/stack_depth_test.py
@@ -1,23 +1,30 @@
 # -*- coding: utf-8; -*-
 from thompson.literals import NumberVal
 from thompson.builtin_operators import ArithAdd
-import pytest
+from thompson.context import Context
+from thompson.bindings import Binding
+from thompson.evaluators import evaluate
 
 
-@pytest.mark.skip
-def test_stack_depth(empty_context_eval):
-    E = empty_context_eval
+def main():
+    c = Context(Binding())
+
+    def E(expr):
+        evaluate(c, expr)
+
     N = NumberVal
     count = 0
     BASE = ArithAdd(N(1), N(1))
     cur = BASE
     while True:
         try:
-            result = E(cur)
-            assert result == N(count + 2)
+            E(cur)
             count = count + 1
             cur = ArithAdd(N(1), cur)
         except:
             break
     print("max = ", count)
-    assert None is count
+
+
+if __name__ == '__main__':
+    main()
